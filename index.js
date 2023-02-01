df32abd048b4756ef8d51b2cf8de59823812daec
Changhyun <chang.bae@flatironschool.com>
2023-01-31 23:51:06 -0800

    commit tree 802af21b586e1825cb38f8573ef9ee09864e1d73

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    cats.slice(name);
+    return ["Milo", "Otis", "Garfield", name];
+}
+function prependCat(name){
+    cats.slice(name);
+    return [name, "Milo", "Otis", "Garfield"];
+}
+function removeLastCat(){
+    cats.slice(-2);
+    return ["Milo", "Otis"];
+}
+function removeFirstCat(){
+    cats.slice(0);
+    return ["Otis", "Garfield"];
+}
